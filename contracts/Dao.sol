fe5d10319297a7a2f1a18ee7be13e8adfbd6f152
tridetch <tridetch@gmail.com>
2022-04-19 20:23:07 +0400

    finish smart contract methods

--- contracts/Dao.sol
+++ contracts/Dao.sol
@@ -5,6 +5,21 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+///@dev Unable to withdraw while there is a vote in which the user participated
+error VotingInProgress();
+///@dev User already voted on this proposal
+error AlreadyVoted();
+///@dev Incorrect quorum percent. Must be in range 0-100
+error IncorrectQuorumParameter();
+///@dev User try to vote on proposal where debating perioud is over
+error VotePeriodIsOver();
+///@dev Proposal with such id do not exist
+error ProposalDoNotExist();
+///@dev This proposal already has been executed
+error ProposalAlreadyExecuted();
+///@dev Debating period for this proposal not over
+error DebatingPeriodNotOver();
+
 contract Dao is ERC20, AccessControl {
     ///@dev Emitted when user deposit tokens in DAO
     ///@param from user address
@@ -16,6 +31,12 @@ contract Dao is ERC20, AccessControl {
     ///@param amount amount of tokens
     event Withdraw(address indexed to, uint256 amount);
 
+    ///@dev Emitted when new proposal created
+    ///@param id id of proposal
+    ///@param recipient address to which the call of instruction will be made
+    ///@param description text description of proposal
+    ///@param endTime deadline for voting
+    ///@param minimumQuorum minimum percent of votes that must participate in voting
     event NewProposal(
         uint256 indexed id,
         address indexed recipient,
@@ -24,26 +45,61 @@ contract Dao is ERC20, AccessControl {
         uint256 minimumQuorum
     );
 
-    ///@dev Unable to withdraw while there is a vote in which the user participated
-    error VotingInProgress();
+    ///@dev Emitted when new vote made for proposal
+    ///@param account address of voted account
+    ///@param decision is user vote for of against the proposal
+    ///@param votingPower amount of tokens that user deposit in dao
+    event Vote(
+        uint256 indexed proposalId,
+        address indexed account,
+        bool indexed decision,
+        uint256 votingPower
+    );
 
-    ///@dev Share in the DAO
+    ///@dev Emitted when proposal finished
+    ///@param proposalId id of proposal
+    ///@param result result of proposal voting and executing
+    event ProposalFinished(uint256 indexed proposalId, ProposalResult indexed result);
+
+    ///@dev Struct that contin information about user share of DAO
+    ///@param amount amount of tokens staked by user
+    ///@param lockedUntil Timestamp of last voting, where user participate, will be ended
     struct Share {
         uint256 amount;
-        uint256 lockedUntil; /// Timestamp of last voting, where user participate, will be ended
+        uint256 lockedUntil;
     }
 
-    ///@dev Information about proposal
+    /**
+        @dev This struct contain information about proposal
+        @param instruction instruction that will be executed if proposal pass
+        @param recepient target address for instruction
+        @param description text description of proposal
+        @param endTime deadline for voting
+        @param minimumQuorum minimum number of votes for a vote to be considered successful
+        @param voter addresses that have already voted
+        @param votesFor total votes that support proposal
+        @param votesAgainst total votes that against proposal
+        @param isFinished true if finish already called for proposal, false othervise
+    */
     struct Proposal {
         bytes instruction;
         address recipient;
         string description;
         uint256 endTime;
         uint256 minimumQuorum;
-        mapping(address => bool) voters; /// Who already voted
-        uint256 voteFor; /// Total positive votes
-        uint256 voteAgainst; /// Total negative votes
-        bool executed;
+        mapping(address => bool) voters;
+        uint256 votesFor;
+        uint256 votesAgainst;
+        bool isFinished;
+    }
+
+    ///@dev Status of proposal
+    ///@param ///voting period
+    enum ProposalResult {
+        QuorumNotPass,
+        Rejected,
+        Executed,
+        ExecutedWithError
     }
 
     /// Person who is allowed to create new proposals
@@ -51,7 +107,7 @@ contract Dao is ERC20, AccessControl {
     bytes32 public constant CHAIRPERSON_ROLE = keccak256("CHAIRPERSON_ROLE");
 
     ///@dev Proposal id
-    uint256 public proposalId;
+    uint256 public nextProposalId;
 
     ///@dev User deposits for participating in voting
     mapping(address => Share) public shares;
@@ -59,20 +115,51 @@ contract Dao is ERC20, AccessControl {
     ///@dev Proposal id to proposal info
     mapping(uint256 => Proposal) public proposals;
 
-    /// The number of votes required for a vote to be considered valid
+    /// Number of votes required for a proposal to be considered valid
     uint256 public minimumQuorum;
     /// Period of voting
     uint256 public debatingPeriodDuration;
 
-    constructor(uint256 minimumQuorum_, uint256 debatingPeriodDuration_) ERC20("Governance", "GovToken") {
-        minimumQuorum = minimumQuorum_;
+    ///@dev constructor for contract
+    ///@param minimumQuorumPercent_ percent of voters that must particiate in voting
+    ///@param debatingPeriodDuration_ period of voting
+    ///@param daoTokenSupply_ total supply of dao tokens
+    constructor(
+        uint256 minimumQuorumPercent_,
+        uint256 debatingPeriodDuration_,
+        uint256 daoTokenSupply_
+    ) ERC20("Governance", "GovToken") {
+        minimumQuorum = calculateQuorumAmount(minimumQuorumPercent_, daoTokenSupply_);
         debatingPeriodDuration = debatingPeriodDuration_;
         chairperson = msg.sender;
         _grantRole(CHAIRPERSON_ROLE, msg.sender);
+        _mint(msg.sender, daoTokenSupply_);
     }
 
-    function mint(address to, uint256 amount) external {
-        _mint(to, amount);
+    ///@dev Calculate quorum votes amount from total supply and percent
+    ///@param quorumPercent percent of votes that must vote on proposal
+    ///@param tokenSupply total token supply
+    function calculateQuorumAmount(uint256 quorumPercent, uint256 tokenSupply)
+        private
+        pure
+        returns (uint256 quorum)
+    {
+        if (quorumPercent > 100) revert IncorrectQuorumParameter();
+        if (quorumPercent == 0) {
+            quorum = 0;
+        } else {
+            quorum = ((tokenSupply * quorumPercent) / 100);
+        }
+    }
+
+    ///@dev Set new quorum percent
+    ///@param quorumPercent quorum in percents
+    function setQuorum(uint256 quorumPercent) external onlyRole(CHAIRPERSON_ROLE) {
+        minimumQuorum = calculateQuorumAmount(quorumPercent, totalSupply());
+    }
+
+    function setDebatingDuration(uint256 duration) external onlyRole(CHAIRPERSON_ROLE) {
+        debatingPeriodDuration = duration;
     }
 
     ///@dev Deposit funds in DAO for voting
@@ -108,15 +195,81 @@ contract Dao is ERC20, AccessControl {
         address recipient,
         string memory description
     ) external onlyRole(CHAIRPERSON_ROLE) {
-        Proposal storage proposal = proposals[proposalId];
-
+        Proposal storage proposal = proposals[nextProposalId];
         proposal.instruction = instruction;
         proposal.recipient = recipient;
         proposal.description = description;
         proposal.endTime = block.timestamp + debatingPeriodDuration;
         proposal.minimumQuorum = minimumQuorum;
 
-        emit NewProposal(proposalId, recipient, description, block.timestamp + debatingPeriodDuration, minimumQuorum);
-        proposalId++;
+        emit NewProposal(
+            nextProposalId,
+            recipient,
+            description,
+            block.timestamp + debatingPeriodDuration,
+            minimumQuorum
+        );
+
+        nextProposalId++;
+    }
+
+    ///@dev Check wether proposal with such id exist or not
+    ///@param proposalId id of proposal to be verified
+    ///@return true if proposal exist, false othervise
+    function isProposalNotExist(uint256 proposalId) private view returns (bool) {
+        return proposalId >= nextProposalId;
+    }
+
+    ///@dev Vote on proposal
+    ///@param proposalId id of proposal in with user wants participate
+    ///@param decision user decision, true if accepted by user, false otherwise
+    function vote(uint256 proposalId, bool decision) external {
+        if (isProposalNotExist(proposalId)) revert ProposalDoNotExist();
+        Proposal storage proposal = proposals[proposalId];
+        if (proposal.voters[msg.sender]) revert AlreadyVoted();
+        if (proposal.endTime < block.timestamp) revert VotePeriodIsOver();
+
+        uint256 votingPower = shares[msg.sender].amount;
+        if (decision) {
+            proposal.votesFor += votingPower;
+        } else {
+            proposal.votesAgainst += votingPower;
+        }
+
+        proposal.voters[msg.sender] = true;
+        emit Vote(proposalId, msg.sender, decision, votingPower);
+    }
+
+    ///@dev Finish proposal
+    ///@param proposalId id of proposal
+    function finishProposal(uint256 proposalId) external {
+        if (isProposalNotExist(proposalId)) revert ProposalDoNotExist();
+        Proposal storage proposal = proposals[nextProposalId];
+        if (proposal.endTime > block.timestamp) revert DebatingPeriodNotOver();
+        if (proposal.isFinished) revert ProposalAlreadyExecuted();
+
+        uint256 totalVotes = proposal.votesFor + proposal.votesAgainst;
+
+        if (totalVotes < proposal.minimumQuorum) {
+            emit ProposalFinished(proposalId, ProposalResult.QuorumNotPass);
+        } else if (proposal.votesAgainst >= proposal.votesFor) {
+            emit ProposalFinished(proposalId, ProposalResult.Rejected);
+        } else {
+            executeProposal(proposalId, proposal.instruction, proposal.recipient);
+        }
+        proposal.isFinished = true;
+    }
+
+    function executeProposal(
+        uint256 proposalId,
+        bytes memory instruction,
+        address recipient
+    ) private {
+        (bool success, ) = recipient.call(instruction);
+        if (success) {
+            emit ProposalFinished(proposalId, ProposalResult.Executed);
+        } else {
+            emit ProposalFinished(proposalId, ProposalResult.ExecutedWithError);
+        }
     }
 }
--- test/Dao.ts
+++ test/Dao.ts
@@ -2,12 +2,37 @@ import { assert, expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("Dao governance", function () {
-    describe("Deploy", function () {
-        it("Should setup initial parameters correctly", async () => {
-            // minimumQuorum;
-            // debatingPeriodDuration;
-            // chairperson;
-            assert(false);
+    describe("Deploy and common methods", function () {
+        describe("#constructor()", function () {
+            it("Should setup initial parameters correctly", async () => {
+                // minimumQuorum;
+                // debatingPeriodDuration;
+                // chairperson;
+                // calculate correct quorum
+                assert(false);
+            });
+            it("Should fail if parameter 'quorumPercent' not in range 0 - 100 percents", async () => {
+                assert(false);
+            });
+        });
+        describe("#setQuorum()", function () {
+            it("Should change quorum", async () => {
+                assert(false);
+            });
+            it("Should fail if parameter 'quorumPercent' not in range 0 - 100 percents", async () => {
+                assert(false);
+            });
+            it("Only chairperson", async () => {
+                assert(false);
+            });
+        });
+        describe("#setDebatingDuration()", function () {
+            it("Should change duration", async () => {
+                assert(false);
+            });
+            it("Only chairperson", async () => {
+                assert(false);
+            });
         });
     });
 
@@ -31,6 +56,9 @@ describe("Dao governance", function () {
                 assert(false);
             });
         });
+    });
+
+    describe("Voting", function () {
         describe("#addProposal()", function () {
             it("Should fail if not a chairman", async () => {
                 assert(false);
@@ -39,6 +67,46 @@ describe("Dao governance", function () {
                 //emit event
                 //proposal added to mapping
                 //id increased
+                assert(false);
+            });
+        });
+        describe("#vote()", function () {
+            it("Should fail if no such proposal", async () => {
+                assert(false);
+            });
+            it("Should fail if already voted", async () => {
+                assert(false);
+            });
+            it("Should fail if debatingPeriod over", async () => {
+                assert(false);
+            });
+            it("Should count user vote", async () => {
+                ///add voter to list
+                ///vote for, vote against
+                assert(false);
+            });
+            it("Should be able to vote on all ongoing proposals", async () => {
+                ///add voter to list
+                assert(false);
+            });
+        });
+        describe("#finishProposal()", function () {
+            it("Should fail if already executed", async () => {
+                assert(false);
+            });
+            it("Should fail if no such proposal", async () => {
+                assert(false);
+            });
+            it("Should fail if debating in progress", async () => {
+                assert(false);
+            });
+            it("Should be finished with quorum failed event", async () => {
+                assert(false);
+            });
+            it("Should be finished with rejected event", async () => {
+                assert(false);
+            });
+            it("Should be finished with accepted event, and execute instruction", async () => {
                 assert(false);
             });
         });
